92f55ca558e87ffb1c9f4285248ca9a6f67f1b97
DiogoAlexandrinoDias <alexandrinodiasd@gmail.com>
2025-09-15 21:52:45 -0300

    aula dia 17°

---
+++ Aula 14° de Front/js/script.js
@@ -0,0 +1,5 @@
+let minhavariavel ;
+minhavariavel = 10
+ ;
+console.log(minhavariavel) ;
+alert(minhavariavel) ;
---
+++ Aula 15° de Front/script.js
@@ -0,0 +1,17 @@
+
+
+// declarar variaveis
+let textoa = "Texto A";
+let textob = "Texto B";
+let textoc = "Texto C";
+let textod = "Texto D";
+let textoe = "Texto E";
+
+// mostrar no console
+console.log(textob);
+console.log(textoc);
+console.log(textod);
+console.log(textoe);
+
+ 
+alert (textoa)
